0c2f18764400665386f7330219a411bd9697a6d1
Hiruth Marie Stautland <hiruthstautland@gmail.com>
2020-08-19 18:30:18 +0200

    add style

--- src/scripts/components/ViewButtons/index.js
+++ src/scripts/components/ViewButtons/index.js
@@ -2,30 +2,31 @@ import React from "react";
 import "./style";
 
 export const ViewButtons = () => (
-  <div className="container ">
-    <section className="buttons">
-      <h1>Button Hover Effects</h1>
-      <div className="container__btns">
-        <a href="https://twitter.com/Dave_Conner" className="btn btn-1">
-          <svg>
-            <rect x="0" y="0" fill="none" width="100%" height="100%" />
-          </svg>
-          Hover
-        </a>
-        {/* <!--svg hover inspired by https://codepen.io/karimbalaa/pen/qERbBY?editors=110 --> */}
-        <a href="#" className="btn btn-2">
-          Hover
-        </a>
-        <a href="#" className="btn btn-3">
-          Hover
-        </a>
-        <a href="#" className="btn btn-4">
-          <span>Hover</span>
-        </a>
-        <a href="#" className="btn btn-5">
-          Hover
-        </a>
-      </div>
-    </section>
-  </div>
+  <section className="container__buttons">
+    <h1>Button Hover Effects</h1>
+    <div className="container__buttons-hover">
+      <a href="https://twitter.com/Dave_Conner" className="buttons btn-1">
+        <svg>
+          <rect x="0" y="0" fill="none" width="100%" height="100%" />
+        </svg>
+        Hover
+      </a>
+      <a href="#" className="buttons btn-2">
+        Hover
+      </a>
+      <a href="#" className="buttons btn-3">
+        Hover
+      </a>
+      <a href="#" className="buttons btn-4">
+        <span>Hover</span>
+      </a>
+      <a href="#" className="buttons btn-5">
+        <span>H</span>
+        <span>O</span>
+        <span>V</span>
+        <span>e</span>
+        <span>r</span>
+      </a>
+    </div>
+  </section>
 );

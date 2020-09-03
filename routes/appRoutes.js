8d7abcb1b187c528ab579499a5d893abd0f5e29b
Hiruth Marie Stautland <hiruthstautland@gmail.com>
2020-09-03 11:18:26 +0200

    added sentry to the back

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -1,18 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const Sentry = require("@sentry/node");
+const Tracing = require("@sentry/tracing");
 const getOrganization = require("./../services/getOrganization");
 // service function
 
 Sentry.init({
-  dsn: process.env.SENTRY_DSN,
+  dsn: process.env.SENTRY_DSN_BACK,
+  tracesSampleRate: 1.0,
+});
+
+const transaction = Sentry.startTransaction({
+  op: "test",
+  name: "My First Test Transaction",
 });
 
 router.post("/", async (req, res, next) => {
   const orgArr = req.body;
   try {
     let organizationInfo = await getOrganization(orgArr);
+    console.log(organizationInfo);
     res.status(200).send(organizationInfo);
+    // throw new Error("TEST from back");
   } catch (error) {
     Sentry.captureException(error);
     res.status(400).send(`Couldn't get information: ${error}`);

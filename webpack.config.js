d32e6062dad77b53abf57b33cddf8e6775e0f01c
Hiruth Marie Stautland <hiruthstautland@gmail.com>
2020-09-03 10:48:51 +0200

    added source map

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,11 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        enforce: "pre",
+        use: ["source-map-loader"],
+      },
+      {
         test: /\.(js|jsx)?$/,
         exclude: /node_modules/,
         use: {
